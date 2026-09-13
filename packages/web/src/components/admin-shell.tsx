98f56aab66463de4116e8b2e3ddfbc0e681686e8
Taras <Taras.Fomin@gmail.com>
2026-09-13 16:42:26 +0300

    fix: narrow t3code versions modal

--- packages/web/src/components/admin-shell.tsx
+++ packages/web/src/components/admin-shell.tsx
@@ -241,7 +241,7 @@ function T3CodeUpdatesDialog({
           onOpenChange(nextOpen);
         }}
       >
-        <DialogPopup className="max-w-5xl">
+        <DialogPopup className="max-w-4xl">
           <DialogHeader>
             <DialogTitle>T3 Code Versions</DialogTitle>
             <DialogDescription>
